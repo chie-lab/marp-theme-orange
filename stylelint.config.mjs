7e6eb3975a2dbfd4dd4698d83226494226283918
Koshiro Okada <koshiro.okada.30@gmail.com>
2025-12-12 22:03:05 +0900

    style: prettier:fix

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import("stylelint").Config} */
 export default {
-  "plugins": ["stylelint-prettier"],
-  "extends": ["stylelint-config-standard-scss"],
-"rules": {
-    "prettier/prettier": true
-  }
+  plugins: ["stylelint-prettier"],
+  extends: ["stylelint-config-standard-scss"],
+  rules: {
+    "prettier/prettier": true,
+  },
 };
